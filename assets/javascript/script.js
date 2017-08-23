94dfdbde9f9508b07b2c205512a7066cc107a88b
joeyfboone <joey@urgentmail.com>
2017-08-23 15:37:35 -0400

    initial js with pseudo code

---
+++ assets/javascript/script.js
@@ -0,0 +1,25 @@
+
+//Letter choices available
+var AiChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//Setting starting values
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
+
+//Computer/AI selecting a random letter
+var AiGuess = AiChoices[Math.floor(Math.random() * AiChoices.length)];
+console.log (AiGuess);
+
+//Need to Code recording the Users Guess
+
+
+
+//Need to change guesses and guesses left
+
+//Need to Compare Guess with AI Letter to determine win or loss.
+
+ 
