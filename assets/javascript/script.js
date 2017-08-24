6b7b2e50abb15e18b66277b5b82155881c4e9ac9
joeyfboone <joey@urgentmail.com>
2017-08-23 20:08:54 -0400

    almost final

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,25 +1,86 @@
-
 //Letter choices available
 var AiChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 //Setting starting values
 var wins = 0;
 var losses = 0;
-var guesses = 9;
-var guessesLeft = 9;
 var guessedLetters = [];
-var letterToGuess = null;
-
-//Computer/AI selecting a random letter
-var AiGuess = AiChoices[Math.floor(Math.random() * AiChoices.length)];
-console.log (AiGuess);
-
-//Need to Code recording the Users Guess
+var guessesLeft = 9;
+var guesses = 9;
 
 
+//START OVER FUNCTION
+var start_over = function () {
+    totalGuesses = 9;
+    guessesLeft = 9;
+    guessedLetters = [];
 
-//Need to change guesses and guesses left
+    updateGuessesSelected();
+    updateLetterToGuess();
+    updateGuessesLeft();
+}
 
-//Need to Compare Guess with AI Letter to determine win or loss.
+//Allow 9 Guesses
+var updateGuessesLeft = function () {
+    //HTML for Guesses Left
+    document.querySelector('#guessLeft').innerHTML = "Guesses Left: " + guessesLeft;
+    console.log(AiGuess);
+}
+var updateLetterToGuess = function () {
+    this.AiGuess = this.AiChoices[Math.floor(Math.random() * this.AiChoices.length)];
+
+};
+var updateGuessesSelected = function () {
+    // Display attempted guesses as series of letters. 
+    document.querySelector('#let').innerHTML = "Your Guesses: " + guessedLetters.join(', ');
+};
+
+//These trigger functions
+updateLetterToGuess();
+updateGuessesLeft();
+
+//User hits selected key to make guess
+
+document.onkeyup = function (event) {
+    guessesLeft--;
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    guessedLetters.push(userGuess);
+    updateGuessesLeft();
+    updateGuessesSelected();
+
+    if (guessesLeft > 0) {
+        if (userGuess == AiGuess) {
+            wins++;
+            document.querySelector('#wins').innerHTML = "Wins: " + wins;
+            alert("Wow, you are psychic!");
+            start_over();
+        }
+    } else if (guessesLeft == 0) {
+        // Then we lose and we'll update the html to display the loss 
+        losses++;
+        document.querySelector('#losses').innerHTML = "Losses: " + losses;
+        alert("Sorry, you are not psychic?");
+        // Then we'll startover. 
+        start_over();
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  
